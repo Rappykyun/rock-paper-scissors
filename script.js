cb91b2a739d41c58c5ea2e5c426134b725397d5a
Rappykyun <rakuichijo155@gmail.com>
2024-06-26 16:46:30 +0800

    completed neccesary features

--- script.js
+++ script.js
@@ -1,52 +1,70 @@
-function getComputerChoice(){
-    const pick = ['rock','paper','scissors'];
-    const randomIndex = Math.floor(Math.random()*pick.length);
-    return pick[randomIndex];
-}
-function getHumanChoice(){
-    const humanChoice = prompt("Please enter youre move: ")
-    return humanChoice;
-}
-function playRound() {
-    let human = getHumanChoice();
-    let computer = getComputerChoice();
-    human = human.toLowerCase(); // Convert human input to lowercase and reassign
+const btnRock = document.getElementById('rock');
+const btnPaper = document.getElementById('paper');
+const btnScissors = document.getElementById('scissors');
+const resultMessage = document.getElementById('resultMessage');
+const humanScores = document.getElementById('humanScore');
+const computerScores = document.getElementById('computerScore');
 
-    if (human === "rock" && computer === "paper") {
-        computerScore++;
-        return console.log("You lose! Paper beats Rock");
-    } else if (human === "paper" && computer === "rock") {
-        humanScore++;
-        return console.log("You Win! Paper beats Rock");
-    } else if (human === "paper" && computer === "scissors") {
-        computerScore++;
-        return console.log("You lose! Scissors beats Paper");
-    } else if (human === "scissors" && computer === "paper") {
-        humanScore++;
-        return console.log("You Win! Scissors beats Paper");
-    } else if (human === "rock" && computer === "scissors") {
-        humanScore++;
-        return console.log("You Win! Rock beats Scissors");
-    } else if (human === "scissors" && computer === "rock") {
-        computerScore++;
-        return console.log("You lose! Rock beats Scissors");
-    } else {
-        return console.log("It's a tie! Both chose " + human.charAt(0).toUpperCase() + human.slice(1));
-    }
-}
-
-function playGame() {
-    for(let i = 1;i<=5;i++){
-        playRound()
-    }
-    if (humanScore > computerScore){
-        return console.log(`Congrats You Win!\n Your Score: ${humanScore}\n Computer score: ${computerScore}`)
-    } else {
-        return console.log(`Sorry You lose!\n Your Score: ${humanScore}\n Computer score: ${computerScore}`)
-    }
-
-}
+btnRock.addEventListener('click', function() {
+    playRound('rock');
+});
+btnPaper.addEventListener('click', function() {
+    playRound('paper');
+});
+btnScissors.addEventListener('click', function() {
+    playRound('scissors');
+});
 
 let humanScore = 0;
 let computerScore = 0;
-playGame();
+
+function getComputerChoice() {
+    const pick = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * pick.length);
+    return pick[randomIndex];
+}
+
+function playRound(human) {
+    let computer = getComputerChoice();
+    human = human.toLowerCase(); 
+
+    if (human === "rock" && computer === "paper") {
+        computerScore++;
+        resultMessage.innerText = "You lose! Paper beats Rock";
+    } else if (human === "paper" && computer === "rock") {
+        humanScore++;
+        resultMessage.innerText = "You Win! Paper beats Rock";
+    } else if (human === "paper" && computer === "scissors") {
+        computerScore++;
+        resultMessage.innerText = "You lose! Scissors beats Paper";
+    } else if (human === "scissors" && computer === "paper") {
+        humanScore++;
+        resultMessage.innerText = "You Win! Scissors beats Paper";
+    } else if (human === "rock" && computer === "scissors") {
+        humanScore++;
+        resultMessage.innerText = "You Win! Rock beats Scissors";
+    } else if (human === "scissors" && computer === "rock") {
+        computerScore++;
+        resultMessage.innerText = "You lose! Rock beats Scissors";
+    } else {
+        resultMessage.innerText = "It's a tie! Both chose " + human.charAt(0).toUpperCase() + human.slice(1);
+    }
+
+    humanScores.innerText = humanScore;
+    computerScores.innerText = computerScore;
+
+    if (humanScore === 5) {
+        resultMessage.innerText = "Congratulations! You win the game!";
+        resetGame();
+    } else if (computerScore === 5) {
+        resultMessage.innerText = "Sorry! You lose the game!";
+        resetGame();
+    }
+}
+
+function resetGame() {
+    humanScore = 0;
+    computerScore = 0;
+    humanScores.innerText = humanScore;
+    computerScores.innerText = computerScore;
+}
